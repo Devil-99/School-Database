ab789155317403977c16cf8c9632fa07e0b117cd
Devil-99 <souvikmaji1999@gmail.com>
2023-01-02 11:10:59 +0530

    Deployed to server, basic required changes done

--- src/collections.js
+++ src/collections.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './collections.css';
 
 import StudentsCollection from './studentsCollection';
 import TeachersCollection from './teachersCollection';
 
 function Collections() {
+  const [teachersDisplay, setTeacherDisplay] = useState(false);
+  const [studentDisplay, setStudentDisplay] = useState(false);
+
   return (
-    
     <div className="buttons">
-    <TeachersCollection/>
-    <StudentsCollection/>
+      <button onClick={()=>setTeacherDisplay(!teachersDisplay)}>displayTeachers</button>
+      {teachersDisplay && <TeachersCollection/>}
+      <button onClick={()=>setStudentDisplay(!studentDisplay)}>displayStudents</button>
+      {studentDisplay && <StudentsCollection/>}
     </div>
 
   )
